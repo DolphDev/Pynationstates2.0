fc7950733c32482cde50db9a346ae4a16f2abfce
DolphDev <dolphdevgithub@gmail.com>
2018-06-05 23:08:57 -0400

    Added the ability to use a specialized dict

--- nsapiwrapper/utils.py
+++ nsapiwrapper/utils.py
@@ -3,31 +3,31 @@
 from xmltodict import parse
 
 
-def _parsedict(x):
+def _parsedict(x, dicttype=dict):
     """
-    This function recursively loops through the processed xml (now dict)
-    it unorderers OrderedDicts and converts them to regular dictionaries
+    This function recursively loops through the processed xml (now dicttype)
+    it unorderers Ordereddicttypes and converts them to regular dicttypeionaries
     """
     if isinstance(x, list):
-        gen_list = [dict(_parsedict(y)) if isinstance(
-            _parsedict(y), dict) else _parsedict(y) for y in x]
+        gen_list = [dicttype(_parsedicttype(y)) if isinstance(
+            _parsedicttype(y), dicttype) else _parsedicttype(y) for y in x]
         return gen_list
     if isinstance(x, str):
         return x
-    if isinstance(x, dict):
-        newdict = {}
+    if isinstance(x, dicttype):
+        newdicttype = {}
         for key in x.keys():
             if key[0] in ["@", "#"]:
                 thiskey = key[1:].lower()
             else:
                 thiskey = key.lower()
-            this_lower = _parsedict(x[key])
-            newdict[thiskey] = dict(this_lower) if isinstance(
-                this_lower, dict) else this_lower
-        return newdict
+            this_lower = _parsedicttype(x[key])
+            newdicttype[thiskey] = dicttype(this_lower) if isinstance(
+                this_lower, dicttype) else this_lower
+        return newdicttype
     if x is None:
         return None
 
 def parsetree(xml):
-    """Converts xml to a simple dictionary"""
-    return _parsedict(parse(xml))
+    """Converts xml to a simple dicttypeionary"""
+    return _parsedicttype(parse(xml))
